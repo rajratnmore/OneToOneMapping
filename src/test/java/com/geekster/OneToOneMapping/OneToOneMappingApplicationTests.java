dbb7ce52ab36163a6ff268ef8f337cdf4f707636
Rajratn More <rajmorecsn@gmail.com>
2023-10-03 09:15:23 +0530

    Project created and pushed

---
+++ src/test/java/com/geekster/OneToOneMapping/OneToOneMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.OneToOneMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
